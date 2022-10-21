ed8076f761e3b7a80f3b2fa502fa39b951c02555
karoljanic97 <karoljanic97@gmail.com>
2022-10-21 10:01:54 +0200

    Added main function

--- src/main/java/org/karoljanic/gitTest/HelloWorld.java
+++ src/main/java/org/karoljanic/gitTest/HelloWorld.java
@@ -3,5 +3,7 @@ package org.karoljanic.gitTest;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("Goodbye World!");
     }
 }
